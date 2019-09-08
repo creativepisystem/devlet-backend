b58ec4e6e6e73e0cb525d279819729e5116103a2
leoferreiralima <leoferreiralima@yahoo.com>
2019-09-08 15:04:43 -0300

    Mais ajustes

---
+++ src/main/java/br/com/creative/devlet/service/UserServiceImpl.java
@@ -0,0 +1,44 @@
+package br.com.creative.devlet.service;
+
+import br.com.creative.devlet.entity.User;
+import br.com.creative.devlet.repo.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+import java.util.Optional;
+
+public class UserServiceImpl implements UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public Optional<User> getUserById(Long id) {
+        return userRepository.findById(id);
+    }
+
+    @Override
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    @Override
+    public User createUser(User user) {
+        return userRepository.save(user);
+    }
+
+    @Override
+    public User updateUser(User user) {
+        return userRepository.save(user);
+    }
+
+    @Override
+    public void deleteUser(Long userId) {
+        userRepository.deleteById(userId);
+    }
+
+    @Override
+    public User findByUsername(String username) {
+        return userRepository.findByUsername(username);
+    }
+}
