3f53eb07a2d24b183d47a3fa3baf32568894cb57
ElielC <eliel.silveira@fatec.sp.gov.br>
2019-10-17 11:05:51 -0300

    team enterpriseId fix

--- src/main/java/br/com/creative/devlet/service/TeamService.java
+++ src/main/java/br/com/creative/devlet/service/TeamService.java
@@ -15,12 +15,14 @@ import java.util.Optional;
 public interface TeamService {
     BussinessException PERSON_DOESNT_EXIST_EXCEPTION = new BussinessException("The selected person doesn't exist");
     BussinessException TEAM_DOESNT_EXIST_EXCEPTION = new BussinessException("The selected team doesn't exist");
+    BussinessException Enterprise_DOESNT_EXIST_EXCEPTION = new BussinessException("The selected enterprise doesn't exist");
     BussinessException PERSON_ALREADY_IN_TEAM_EXCEPTION = new BussinessException("The selected person is already inside the team");
     BussinessException TEAM_AND_USER_NOT_IN_SAME_ENTERPRISE_EXCEPTION = new BussinessException("The team and the user are not in the same enterprise");
     BussinessException PERSON_AND_USER_NOT_IN_SAME_ENTERPRISE_EXCEPTION = new BussinessException("The person and the user are not in the same enterprise");
     BussinessException THERE_ARE_NO_PEOPLE_IN_THE_TEAM_EXCEPTION = new BussinessException("The team doesn't have any people in it");
     BussinessException PERSON_IS_NOT_IN_TEAM_EXCEPTION = new BussinessException("The selected person is not in the team");
     BussinessException TEAM_OR_PERSON_ID_ARE_INVALID_EXCEPTION = new BussinessException("The Person and Team id must be not null and positive");
+
 
     GetTeamModel findById(Long id) throws BussinessException;
 
