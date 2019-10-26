62b8668dc1d8fffb5a70d2821941454b7ada2f83
ElielC <eliel.silveira@fatec.sp.gov.br>
2019-10-25 21:42:42 -0300

    validation v1

--- src/main/java/br/com/creative/devlet/model/PutStageModel.java
+++ src/main/java/br/com/creative/devlet/model/PutStageModel.java
@@ -1,11 +1,21 @@
 package br.com.creative.devlet.model;
 
+import org.springframework.format.annotation.DateTimeFormat;
+
+import javax.validation.constraints.*;
 import java.util.Date;
 
 public class PutStageModel {
+    @Null(message = "This field must be empty")
     private Long id;
+    @NotBlank(message = "The stage name can't be empty")
+    @Size(min = 3, max = 50, message = "The stage name must be within 3 and 50 characters")
     private String name;
+    @NotNull(message = "The project id can't be null")
+    @Positive(message = "The project id must be positive")
     private String description;
+    @Pattern(regexp = "^((19|2[0-9])[0-9]{2})-(0[1-9]|1[012])-(0[1-9]|[12][0-9]|3[01])$",
+            message = "The date must follow the mask: YYYY-MM-DD")
     private Date date;
 
 
